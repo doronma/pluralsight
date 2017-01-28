7e1685cb6323d10ad267c0040e9591c859bea0ea
Doron Marcus <Doron Marcus>
2017-01-28 18:15:05 +0200

    Section5

---
+++ pluralsight/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import {ProductListComponent } from './product-list/product-list.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Product Management';
+}
