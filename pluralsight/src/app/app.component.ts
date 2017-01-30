e6aeb35504bfd41f5b6eb521ac5bc5e1d9d50cd3
Doron Marcus <Doron Marcus>
2017-01-30 19:59:10 +0200

    section 8

--- pluralsight/src/app/app.component.ts
+++ pluralsight/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './services/product.service'
+
 import {ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   pageTitle = 'Product Management';
