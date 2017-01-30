e6338e308bad48209ec6b20ea855f702bb3cc80a
Doron Marcus <Doron Marcus>
2017-01-30 20:51:30 +0200

    section 9

--- pluralsight/src/app/product-list/product-list.component.ts
+++ pluralsight/src/app/product-list/product-list.component.ts
@@ -17,12 +17,16 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
     listFilter: string ;
     products: IProduct[] ;
+    errorMessage: string;
         
     constructor(private _productService: ProductService) { }
 
     ngOnInit() {
         console.log('In product-list init...');
-        this.products = this._productService.getProducts();
+        //this.products = this._productService.getProducts();
+        this._productService.getProducts()
+        .subscribe(products => this.products = products
+                  ,error => this.errorMessage = <any>error);
     }
 
     toggleImage(): void {
