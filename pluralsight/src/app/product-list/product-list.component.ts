fe757f563187691e434dacc696f849609e83227a
Doron Marcus <Doron Marcus>
2017-02-06 21:00:19 +0200

    add promise http sample

--- pluralsight/src/app/product-list/product-list.component.ts
+++ pluralsight/src/app/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 import { IProduct } from '../product/product';
-import {StarComponent} from '../share/star/star.component'
+import { StarComponent } from '../share/star/star.component'
 import { ProductService } from '../services/product.service'
 
 @Component({
@@ -15,25 +15,39 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string ;
-    products: IProduct[] ;
+    listFilter: string;
+    products: IProduct[];
     errorMessage: string;
-        
+    dataOption: number = 3;
+
     constructor(private _productService: ProductService) { }
 
     ngOnInit() {
         console.log('In product-list init...');
+        //op1
+
         //this.products = this._productService.getProducts();
-        this._productService.getProducts()
-        .subscribe(products => this.products = products
-                  ,error => this.errorMessage = <any>error);
+
+        if (this.dataOption == 2) {
+            // op2
+            this._productService.getProducts()
+                .subscribe(products => this.products = products
+                , error => this.errorMessage = <any>error);
+        } else if (this.dataOption == 3) {
+            // op3
+            this._productService.getProductsWithPromise()
+                .then(products => {
+                    console.log('in then' + products);
+                    this.products = products
+                });
+        }
     }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
-    onRatingClicked(message: string): void{
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product List:' + message;
     }
 
--- pluralsight/src/app/services/product.service.ts
+++ pluralsight/src/app/services/product.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+
 
 import { IProduct } from '../product/product';
 
@@ -12,18 +14,31 @@ import { IProduct } from '../product/product';
 export class ProductService {
 
   constructor(private _http: Http) { }
-  private _productUrl = './res/products.json';
+  //private _productUrl = './res/products.json';
+  private _productUrl = 'http://localhost:8080/products';
 
-  getProducts(): Observable<IProduct[]>{
-     return this._http.get(this._productUrl)
-    .map((response: Response) => <IProduct[]>response.json())
-    .do(data => console.log('All: ' + JSON.stringify(data)))
-    .catch(this.handleError);
-   
+  getProducts(): Observable<IProduct[]> {
+    return this._http.get(this._productUrl)
+      .map((response: Response) => <IProduct[]>response.json())
+      .do(data => console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+
   }
   private handleError(error: Response) {
     console.error(error);
     return Observable.throw(error.json().error || 'Server error');
+  }
+
+  getProductsWithPromise(): Promise<IProduct[]> {
+    {
+      console.log('getProductWithPromise...');
+      const result = this._http.get(this._productUrl)
+        .toPromise()
+        .then(response => response.json() as IProduct[])
+        .catch(this.handleError);
+      console.log('returning result...');
+      return result;
+    }
   }
 
 }
